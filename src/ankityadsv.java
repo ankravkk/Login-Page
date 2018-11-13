0b5426cc1125623c181a58bf525fda50f7d32f66
Ankit Yadav <ankravkk@gmail.com>
2018-11-14 00:03:34 +0530

    first

--- src/ankityadsv.java
+++ src/ankityadsv.java
@@ -23,6 +23,8 @@ public class ankityadsv extends JFrame {
 	private JPanel contentPane;
 	private JTextField user;
 	private JPasswordField password;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -33,19 +35,16 @@ public class ankityadsv extends JFrame {
 				try {
 					ankityadsv frame = new ankityadsv();
 					frame.setVisible(true);
-				} catch (Exception e) {
+				} catch (Exception e)     {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public ankityadsv() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 290, 329);
+		setBounds(100, 100, 480, 331);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -57,11 +56,11 @@ public class ankityadsv extends JFrame {
 		contentPane.add(lblLoginPage);
 		
 		JLabel lblUsername = new JLabel("Username");
-		lblUsername.setBounds(38, 100, 48, 14);
+		lblUsername.setBounds(10, 100, 76, 14);
 		contentPane.add(lblUsername);
 		
 		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setBounds(38, 141, 46, 14);
+		lblPassword.setBounds(10, 141, 74, 14);
 		contentPane.add(lblPassword);
 		
 		user = new JTextField();
@@ -77,7 +76,7 @@ public class ankityadsv extends JFrame {
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
-					Class.forName("com.mysql.jdbc.Driver");
+					Class.forName("java.sql.Driver");
 					Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","root");
 					Statement stmt=con.createStatement();
 					String sql="Select * from tblogin where username='"+user.getText()+"'and password='"+password.getText().toString()+"'";
@@ -90,7 +89,44 @@ public class ankityadsv extends JFrame {
 				}catch(Exception e) {System.out.print(e);}
 			}
 		});
-		btnNewButton.setBounds(58, 179, 130, 23);
+		btnNewButton.setBounds(10, 201, 111, 23);
 		contentPane.add(btnNewButton);
+		
+		JButton btnSignUp = new JButton("Sign up");
+		btnSignUp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					string query="insert into tblogin(username,password) values(?,?)";
+				}
+				catch(Exception e)
+				{
+					
+				}
+			}
+		});
+		btnSignUp.setBounds(298, 201, 105, 23);
+		contentPane.add(btnSignUp);
+		
+		JLabel lblNewUser = new JLabel("New User?");
+		lblNewUser.setBounds(311, 38, 76, 14);
+		contentPane.add(lblNewUser);
+		
+		JLabel lblName = new JLabel("Username");
+		lblName.setBounds(250, 80, 76, 14);
+		contentPane.add(lblName);
+		
+		textField = new JTextField();
+		textField.setBounds(326, 77, 99, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblPassword_1 = new JLabel("Password");
+		lblPassword_1.setBounds(250, 127, 46, 14);
+		contentPane.add(lblPassword_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(326, 124, 99, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
 	}
 }
